49e30c1be5f9c4cd5f097e66a41398fe5607d706
Ray Toal <rtoal@lmu.edu>
2022-10-19 13:02:42 -0700

    Add display and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import "./App.css";
+import { useState } from "react";
+import TodoList from "./TodoList";
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, content: "Meet Masao for Udon" },
+    { id: 2, content: "Fix up these horrible React notes" },
+  ]);
+  function deleteTodo(id) {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }
   return (
     <div className="App">
-      <header className="App-header">TODO LIST</header>
+      <h1>Things To Do</h1>
+      <TodoList todos={todos} deleter={deleteTodo} />
     </div>
   );
 }
-
-export default App;
