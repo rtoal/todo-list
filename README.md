8648cbc021a5d6df94c99c58b6611849a2c55058
Ray Toal <rtoal@lmu.edu>
2022-10-24 11:18:47 -0700

    Add delete functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TODO List
 
 A remarkable cool TODO List I made using React ofc
+
+Remember, for homework, flesh this out a lot
--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   const [todos, setTodos] = useState([
     { id: 1, content: "Meet Masao for Udon" },
     { id: 2, content: "Fix up these horrible React notes" },
+    { id: 3, content: "Finish grading!!!" },
   ]);
   function deleteTodo(id) {
     setTodos((todos) => todos.filter((todo) => todo.id !== id));
